7509fc86f0e01d154408712d0bef83cd00c4821b
Kevin Secretan <kevinmccloud@gmail.com>
2011-06-20 07:14:43 -0700

    typo2

--- phpserver/app/services/HomeService.php
+++ phpserver/app/services/HomeService.php
@@ -22,7 +22,7 @@ class HomeService extends BaseAppService {
     $list = $this->get_servers_list();
     $template_data = array(
         'title' => 'LucidDB AdminUI Demo'
-      , 'master' => 'SQLAdmin.tpl'
+      , 'master' => 'master.tpl'
       , 'content' => 'content/home.tpl'
       , 'servers' => $list
     );
